ea90543fc8852b8358d01a1b04ae4060979db4eb
SpiritCroc <spiritcroc@gmail.com>
2016-07-12 09:53:13 +0200

    Revert "set Camera HAL version 1 as default"
    Causes some issues when trying to access flashlight
    
    This reverts commit 43c867123ce8915326a13b951e7f6421adb6a868.

--- common.mk
+++ common.mk
@@ -250,7 +250,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
     camera.disable_zsl_mode=0 \
-    persist.camera.HAL3.enabled=0 \
+    persist.camera.HAL3.enabled=1 \
     persist.camera.ois.disable=0
 
 # Sensors debug
