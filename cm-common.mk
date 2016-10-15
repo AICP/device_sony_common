a7118bc451b554f120e3aa3457613233a437aabe
SpiritCroc <spiritcroc@gmail.com>
2016-10-15 11:28:54 +0200

    Use custom display HAL
    Change-Id: I4ab36c8714cf11725101628bc4e01e56ecfc3e09

--- cm-common.mk
+++ cm-common.mk
@@ -30,3 +30,7 @@ PRODUCT_PACKAGES += \
 # Properties
 PRODUCT_PROPERTY_OVERRIDES += \
     telephony.lteOnGSMDevice=1
+
+# Display HAL
+SONY_AOSP_DISPLAY := true
+$(call project-set-path,qcom-display,hardware/sony/display)
