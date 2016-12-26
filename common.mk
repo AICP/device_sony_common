10e765cfc04f66ba96e3d21cd38c8eefdfc3e7eb
David <Davidteri91@gmail.com>
2016-12-26 15:40:43 +0100

    sony-common: DO NOT COPY common sec_config for msm8974
    it can work on other platforms
    
    Change-Id: I7c702e6b722ee1da979cb3244d39571cbd7835ec
    Signed-off-by: David Viteri <davidteri91@gmail.com>

--- common.mk
+++ common.mk
@@ -60,7 +60,6 @@ PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/rootdir/system/vendor/etc/audio_effects.conf:system/vendor/etc/audio_effects.conf \
     $(COMMON_PATH)/rootdir/system/etc/gps.conf:system/etc/gps.conf \
     $(COMMON_PATH)/rootdir/system/etc/nfcee_access.xml:system/etc/nfcee_access.xml \
-    $(COMMON_PATH)/rootdir/system/etc/sec_config:system/etc/sec_config.bak \
     $(COMMON_PATH)/rootdir/system/etc/sensors/sensors_settings:system/etc/sensors/sensors_settings
 
 # QMI
@@ -283,6 +282,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 else
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.camera.HAL3.enabled=1
+
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/rootdir/system/etc/sec_config:system/etc/sec_config
 endif
 
 # Sensors debug
